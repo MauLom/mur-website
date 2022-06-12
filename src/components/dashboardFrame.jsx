be7eff2ac899604e3e49cbba8ff3210917739d65
Mauricio Lombano <mauricio.lombano@gmail.com>
2022-06-12 13:43:17 -0500

    editados componentes de dashboard, y agregados previes juegos

--- src/components/dashboardFrame.jsx
+++ src/components/dashboardFrame.jsx
@@ -1,31 +1,64 @@
 import * as React from 'react'
+import HeaderOptions from './headerOptions';
 
 ///Bootstrap components
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+///Assets
+import GameBombs from '../assets/BOMBAS.png'
+import GameWheel from '../assets/RULETA.png'
+import GameTower from '../assets/TORRES.png'
+
+
 const DashboardFrame = () => {
+    const styles = {
+        glassSquares: {
+            background: "rgba(231, 231, 231, 0.43)",
+            borderRadius: "41px",
+
+        },
+        imgGames: {
+            width: "100%",
+            height: "100%"
+        },
+        titleHall: {
+            fontStyle: "normal",
+            fontWeight: "400",
+            fontSize: "48px",
+            lineHeight: "72px",
+            letterSpacing: "0.03em",
+            color: "#FFFFFF",
+        }
+    }
+
     return (
         <>
             <Row style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "1474px", height: "60px" }}>
-                <Col>
-                    Usuario
-                </Col>
-                <Col>
-                    <button style={{ background: "rgba(27, 191, 0, 0.92)", borderRadius: "41px", width: "159px", height: "41px", }} >Depositar</button>
-                </Col>
-                <Col>
-                    <button style={{ background: "rgba(27, 191, 0, 0.92)", borderRadius: "41px", width: "159px", height: "41px", }} >Retirar</button>
-                </Col>
+                <HeaderOptions />
             </Row>
-            <Row>
-                <Col className="mt-4" style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "453px", height: "352px" }}>Cosas juegos</Col>
-                <Col className="mt-4" style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "453px", height: "352px" }}>Cosas juegos</Col>
-                <Col className="mt-4" style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "453px", height: "352px" }}>Cosas juegos</Col>
+            <Row className='justify-content-center'>
+                <Col lg={3} className="mx-3 mt-4" style={styles.glassSquares}>
+                    <img style={styles.imgGames} src={GameBombs} alt="game-bombs" />
+                </Col>
+                <Col lg={3} className="mx-3 mt-4" style={styles.glassSquares}>
+                    <img style={styles.imgGames} src={GameWheel} alt="game-bombs" />
+                </Col>
+                <Col lg={3} className="mx-3 mt-4" style={styles.glassSquares}>
+                    <img style={styles.imgGames} src={GameTower} alt="game-bombs" />
+                </Col>
 
             </Row>
             <Row>
-                <Col className="mt-4" style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "1469px", height: "428px" }} >Hall of fame</Col>
+                <Col lg={12}>
+                    <Row className=" text-center mt-4">
+                        <Col><div style={styles.titleHall}>Hall of fame</div></Col>
+                    </Row>
+                    <Row>
+                        <Col className="mt-4" style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "1469px", height: "428px" }} >Contenido tabla</Col>
+                    </Row>
+                </Col>
+
             </Row>
         </>
     )
