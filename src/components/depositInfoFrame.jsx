2d2c3c4b10154012f95504d8f897c0b89d1154ff
Mauricio Lombano <mauricio.lombano@gmail.com>
2022-06-16 19:40:30 -0500

    MODAL

--- src/components/depositInfoFrame.jsx
+++ src/components/depositInfoFrame.jsx
@@ -3,6 +3,8 @@ import * as React from "react"
 ///Bootstrap
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 ///Assetes 
 import QRExample from '../assets/QRExample.png'
@@ -21,30 +23,37 @@ const DepositInfo = () => {
         },
     }
 
+    const [show, setShow] = React.useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <Row>
-            <Col md={1} sm={12} className="text-center mb-2  ">
-                <Row className="mt-2 justify-content-center" >
-                    <img style={styles.logoBTC} alt="BTC-log" src="https://pngimg.com/uploads/bitcoin/bitcoin_PNG36.png" />
-                </Row>
-                <Row className="mt-2 justify-content-center" >
-                    <img style={styles.qrExample} alt="QR-Code" src={QRExample} />
-                </Row>
-            </Col>
-            <Col md={2} sm={2} className="text-center mb-2">
-                <Row className="mt-2"> <div >Wallet</div></Row>
-                <Row className="mt-2">
-                    <div className="font-weight-bold" >
-                        bc1qz8q2yr6lhegxrsckr97kuvdlva8
-                    </div>
-                </Row>
-                <Row className="mt-2">
-                    <div >
-                        Envíe cualquier cantidad de BTC a la siguiente dirección. En el caso de un depósito no instantáneo, se requiere 1 confirmación
-                    </div>
-                </Row>
-            </Col>
-        </Row>
+        <>
+            <Row>
+                <Col md={1} sm={12} className="text-center mb-2  ">
+                    <Row className="mt-2 justify-content-center" >
+                        <img style={styles.logoBTC} alt="BTC-log" src="https://pngimg.com/uploads/bitcoin/bitcoin_PNG36.png" />
+                    </Row>
+                    <Row className="mt-2 justify-content-center" >
+                        <img style={styles.qrExample} alt="QR-Code" src={QRExample} />
+                    </Row>
+                </Col>
+                <Col md={2} sm={2} className="text-center mb-2">
+                    <Row className="mt-2"> <div >Wallet</div></Row>
+                    <Row className="mt-2">
+                        <div className="font-weight-bold" >
+                            bc1qz8q2yr6lhegxrsckr97kuvdlva8
+                        </div>
+                    </Row>
+                    <Row className="mt-2">
+                        <div >
+                            Envíe cualquier cantidad de BTC a la siguiente dirección. En el caso de un depósito no instantáneo, se requiere 1 confirmación
+                        </div>
+                    </Row>
+                </Col>
+            </Row>
+        </>
     )
 }
 
--- src/components/headerOptions.jsx
+++ src/components/headerOptions.jsx
@@ -3,8 +3,15 @@ import * as React from 'react'
 ///Bootstrap components
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Modal from 'react-bootstrap/Modal'
+import TransactionsFrame from './transactionsWindow';
 
 const HeaderOptions = () => {
+
+    const [show, setShow] =React.useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <Row className="mx-3 mt-4" style={{ background: "rgba(231, 231, 231, 0.43)", borderRadius: "41px", width: "1469px", height: "60px", }}>
@@ -12,11 +19,14 @@ const HeaderOptions = () => {
                 Usuario
             </Col>
             <Col className="mt-2 text-center">
-                <button style={{ background: "rgba(0, 209, 121, 0.92)", borderRadius: "41px", width: "130px", height: "41px", }} >Depositar</button>
+                <button onClick={()=>{handleShow()}} style={{ background: "rgba(0, 209, 121, 0.92)", borderRadius: "41px", width: "130px", height: "41px", }} >Depositar</button>
             </Col>
             <Col className="text-center mt-2">
                 <button style={{ background: "rgba(0, 209, 121, 0.92)", borderRadius: "41px", width: "130px", height: "41px", }} >Retirar</button>
             </Col>
+            <Modal show={show} onHide={handleClose}>
+                <TransactionsFrame/>
+            </Modal>
         </Row>
     )
 }
