4ecc52840090e294c90cdefcedcca5feb6f2cfeb
Mauricio <mauricio.lombano@gmail.com>
2023-04-05 23:09:25 -0600

    Deleted unused imports from home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,7 @@
 import * as React from "react"
-import LoginFrame from "../components/loginFrame"
-import RegisterFrame from "../components/registerFrame";
 
 /// Bootstrap components
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';//ddd
 import DashboardFrame from "../components/dashboardFrame";
 ///Other components
 
@@ -14,12 +11,7 @@ const Home = () => {
     return (
 
         <Container fluid >
-            
                 <DashboardFrame/>
-                
-            
-
-
         </Container>
 
     )
